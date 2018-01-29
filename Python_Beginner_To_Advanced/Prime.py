d5b2b7ee5877a3c24754c94c3826921ffd230343
kiran-aeromech <kiran.aeromech@gmail.com>
2018-01-29 18:11:10 +0530

    Adding python

---
+++ Python_Beginner_To_Advanced/Prime.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+def IsPrime(n):
+    for x in range(2, int(n/2+1)):
+        if not n % x:
+            return False;
+        return True;
+def PrimesTo(n):
+    for x in range(2, n):
+        if IsPrime(x):
+            print(x)
+    
